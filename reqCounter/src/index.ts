5c4590ed11ff0f03673f05a24d937b6e759f824e
AlexGr <753464@gmail.com>
2022-09-11 18:10:02 +0300

    1.0.3

--- reqCounter/src/index.ts
+++ reqCounter/src/index.ts
@@ -5,40 +5,57 @@ import path from 'node:path';
 import fs from 'node:fs';
 
 let requestsCount = 0
-const __dirname = path.resolve();
 
 // Starting the http server
 http
 	.createServer(async (req: IncomingMessage, res: ServerResponse) => {
+		const sendText = (msg: string) => {
+			res.setHeader('Content-Type', 'text/html;charset=utf-8')
+			res.write(msg)
+			res.end()
+		}
+		const sendStream = (fileStream: fs.ReadStream) => {
+			res.setHeader('Content-Type', 'text/plain')
+			fileStream.pipe(res)
+			fileStream.on('open', () => console.log('fileStream-open'))
+			fileStream.on('close', () => console.log('fileStream-close'))//при обрыве соединения нужно fileStream закрыть самому
+			res.on('close', () => { console.log('res-close'); fileStream.destroy() })
+			res.on('finish', () => console.log('res-finish'))//при штатном окончании file stream сам закрывается
+		}
+		const sendFavicon = () => {
+			// Location of your favicon in the filesystem.
+			const __dirname = path.resolve();
+			const faviconPath = path.join(__dirname, 'public/icons', 'favicon.ico')
+			const readFavicon = fs.createReadStream(faviconPath)
+			//send favicon
+			res.setHeader('Content-Type', 'image/x-icon')
+			readFavicon.pipe(res)
+		}
 		//get url from req
 		const url = req.url ?? "undefined"
 		//routs and handlers
 		const routes: { [key: string]: any } = {
-			'/': () => res.write(`IT-CAMASUTRA ${requestsCount++}`),
-			'/favicon.ico': () => {
-				res.setHeader('Content-Type', 'image/x-icon')
-				// Location of your favicon in the filesystem.
-				const favicon = path.join(__dirname, 'public/icons', 'favicon.ico')
-				const readFavicon = fs.createReadStream(favicon)
-				readFavicon.pipe(res)								
+			'/': () => sendText(`IT-CAMASUTRA ${requestsCount++}`),
+			'/favicon.ico': sendFavicon,
+			'/fileStream': () => {
+				const fileStream = fs.createReadStream('test.txt')
+				sendStream(fileStream)
 			},
-			"students": () => res.write('STUDENTS'),
-			"courses": () => res.write('FRONT + BACK'),
-			"notFound": () => res.write(`404 NOT FOUND ${url}`),
-			"undefined": () => res.write('404 NO URL'),
+			"/students": () => sendText('STUDENTS'),
+			"notFound": () => { },
+			"undefined": () => { },
 		}
 		//select handler by url		
 		let handler = routes[url]
 		if (!handler) handler = routes["notFound"]
-		//let's start the selected handler
+		//start the selected handler
 		try {
 			handler()
 		} catch (error) {
-			console.error('index error:', error);
+			console.error('handler error:', error)
 		}
-		//send response
-		if (url != "/favicon.ico") res.end()
-	}).listen(process.env.SERVERPORT);
+	}).listen(process.env.SERVERPORT)
+
 //logging start
-console.log(`\x1b[34mHTTP-server started port: ${process.env.SERVERPORT}`);
+console.log(`\x1b[34mHTTP-server started http://localhost:${process.env.SERVERPORT}`)
 
